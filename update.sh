1769812e265f7cc9b14db9d74a039a87b0810748
FuzzyMistborn <fuzzymistborn@protonmail.com>
2021-11-12 01:07:22 -0500

    add drone

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Commit description: " desc
-rm -r public
-hugo
-caddy reload ~/caddy/Caddyfile
-git add . && \
-git add -u && \
-git commit -m "$desc" && \
-git push origin main
